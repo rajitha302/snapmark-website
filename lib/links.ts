06d946b90ebff4edaa5e559cec002df326ffa690
rajitha302 <rajithadisanayaka999@gmail.com>
2026-04-19 08:27:36 +0530

    chore: add link constants and copy logo from extension repo

---
+++ lib/links.ts
@@ -0,0 +1,10 @@
+export const MARKETPLACE_URL =
+  'https://marketplace.visualstudio.com/items?itemName=RajithaDisanayaka.snapmark';
+
+export const GITHUB_URL = 'https://github.com/rajitha302/Snapmark';
+
+export const SHORTCUT_MAC = '⌘⇧A';
+export const SHORTCUT_WIN = 'Ctrl+Shift+A';
+
+export const SITE_URL = 'https://snapmark.netlify.app'; // placeholder until custom domain
+export const SITE_NAME = 'Snapmark';
